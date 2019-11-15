4e7ac9234cd2a25dc4e72958bfd5a7ea81d24ab0
Elena Piaggio <elenapiaggio@gmail.com>
2019-11-15 20:50:50 +0100

    ADD get apply service

--- src/services/applyService.js
+++ src/services/applyService.js
@@ -12,4 +12,13 @@ class ApplyService{
     return this.axios.post('/apply/', body)
     .then(({ data: apply }) => apply);
   }
+
+  async getAllApplies(){
+    return this.apply.get('/applies')
+      .then(({ data: applies }) => applies);
+  }
 }
+
+const applyService = new ApplyService();
+
+export default applyService;
