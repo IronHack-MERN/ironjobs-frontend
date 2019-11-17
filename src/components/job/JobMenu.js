0355d09f0ac280553e2adf036193186cdd96d0fa
Elena Piaggio <elenapiaggio@gmail.com>
2019-11-17 22:52:23 +0100

    ADD post a job link

--- src/components/job/JobMenu.js
+++ src/components/job/JobMenu.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import ListJobs from '../ListJobs';
 
 class JobMenu extends Component {
+
   // eslint-disable-next-line class-methods-use-this
   render() {
+    console.log('*****PASO POR JOB MENU OK ******');
+
     return (
       <div>
-        <h1>JOB MENU</h1>
+        <h1> _ </h1>
+        <Link
+          to={{
+            pathname: '/private/job/post',
+            props: {
+              
+            }
+          }}>
+          Post a job offer
+          </Link>
         <ListJobs />
       </div>
     );
